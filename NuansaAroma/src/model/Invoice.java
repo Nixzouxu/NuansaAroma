09fd635e8b82a76c86c7c868681aacbf07ba4e53
dedeksaleha-ops <dedeksaleha@gmail.com>
2025-11-25 00:28:55 +0700

    Update Invoice.java

--- NuansaAroma/src/model/Invoice.java
+++ NuansaAroma/src/model/Invoice.java
@@ -1,1 +1,30 @@
+package nuansaaroma.model;
+
+import java.util.Date;
+
+/**
+ * Representasi sebuah invoice (nota) yang berisi detail transaksi dan pembayaran.
+ * Invoice menyimpan informasi seperti ID invoice, tanggal cetak, transaksi terkait,
+ * serta metode pembayaran yang digunakan.
+ */
+public class Invoice {
+    private Transaksi transaksi;
+    private Pembayaran pembayaran;
+    private Date tanggalCetak;
+    private String idInvoice;
+
+    /**
+     * Konstruktor untuk membuat objek Invoice baru berdasarkan transaksi dan pembayaran.
+     * ID invoice dibuat secara otomatis menggunakan timestamp agar unik.
+     * Tanggal cetak diset ke waktu saat objek dibuat.
+     *
+     * @param transaksi  objek transaksi yang dicantumkan pada invoice
+     * @param pembayaran metode pembayaran yang digunakan
+     */
+    public Invoice(Transaksi transaksi, Pembayaran pembayaran) {
+        this.transaksi = transaksi;
+        this.pembayaran = pembayaran;
+        this.tanggalCetak = new Date();
+        this.idInvoice = "INV-" + System.currentTimeMillis();
+    }
 
